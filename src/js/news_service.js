02b5ba65a5ac0f0befd75bd505b937fd316035cc
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2022-10-13 19:25:31 +0300

    make-hw-pass-1

---
+++ src/js/news_service.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+export default class NewsApiServer {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+  async fetchImages() {
+    return await axios
+      .get(
+        `https://pixabay.com/api/?key=30152134-ae58c77c91fd8c4469302fe78&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
+      )
+      .then(date => {
+        this.page += 1;
+        return {
+          totalHits: date.data.totalHits,
+          hits: date.data.hits,
+        };
+      })
+      .catch(error => console.log('error', error));
+  }
+  resetCurrentPage() {
+    this.page = 1;
+  }
+  uppdatePage() {
+    return this.page - 1;
+  }
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
---
+++ src/js/renderElemToHTML.js
@@ -0,0 +1,39 @@
+export function rendeElemToHTML(hits) {
+  const gallery = hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<li class="photo-card">
+                    <div class="card-item">
+                            <a href="${largeImageURL}" class="gallery__image">
+                                <img src="${webformatURL}" alt="${tags}" loading="lazy" width="190">
+                            </a>
+                            <ul class="info">
+                                <li class="info-item">
+                                    <b>Likes</b><br>${likes}
+                                </li>
+                                <li class="info-item">
+                                    <b>Views</b><br>${views}
+                                </li>
+                                <li class="info-item">
+                                    <b>Comments</b><br>${comments}
+                                </li>
+                                <li class="info-item">
+                                    <b>Downloads</b><br>${downloads}
+                                </li>
+                            </ul>
+                    </div>
+                </li>`;
+      }
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', gallery);
+  galleryLigthbox.refresh();
+}
